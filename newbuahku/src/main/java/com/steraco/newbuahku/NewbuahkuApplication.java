56637758ec69b27c063e10eaab6b510000a7e74d
Izharuddin <izharuddin1997@gmail.com>
2024-11-17 12:44:35 +0700

    Updated

---
+++ newbuahku/src/main/java/com/steraco/newbuahku/NewbuahkuApplication.java
@@ -0,0 +1,13 @@
+package com.steraco.newbuahku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbuahkuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbuahkuApplication.class, args);
+    }
+
+}
---
+++ newbuahku/src/test/java/com/steraco/newbuahku/NewbuahkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steraco.newbuahku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbuahkuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
